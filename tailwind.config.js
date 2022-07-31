b04e0859fdbdf90636f0f7187c27799225317be6
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-07-31 09:05:31 +0200

    Update tailwind.config.js
    - adding custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,25 @@ module.exports = {
 			backgroundImage: {
 				"splash": "url('/public/images/background-image.png')",
 			},
+			animation: {
+				"bounce-spin": "bounce-spin 1500ms infinite ease-in-out",
+			},
+			keyframes: {
+				"bounce-spin": {
+					"0%": {
+						transform: "translateY(-25%) rotateY(0deg)",
+					},
+					"50%": {
+						transform: "translateY(0) rotateY(180deg)",
+					},
+					"100%": {
+						transform: "translateY(-25%) rotateY(360deg)",
+					},
+				},
+			},
 		},
-	},
-	plugins: [require("@tailwindcss/forms")],
+		plugins: [require("@tailwindcss/forms")],
+	}
 };
 
 
